1189f671dad295adbc20925bdcb596d78aa7a17a
Mr. Paul Ishaili C <paulishaili@gmail.com>
2022-03-25 04:54:56 +0100

    vv

---
+++ data/Filters.js
@@ -0,0 +1,23 @@
+export const ticketFilters = ['Current', 'Previous'];
+export const homeFilters = [
+  {
+    id: 1,
+    name: 'For You',
+    filterBy: ''
+  },
+  {
+    id: 2,
+    name: 'Trending',
+    filterBy: 'trending'
+  },
+  {
+    id: 3,
+    name: 'Categories',
+    filterBy: 'new'
+  },
+  {
+    id: 4,
+    name: 'Live',
+    filterBy: 'live'
+  }
+];
